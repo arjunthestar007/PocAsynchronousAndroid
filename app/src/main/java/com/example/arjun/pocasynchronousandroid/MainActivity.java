62a27bf6417113f8d64c6f5bebc5f7cdf0184861
arjun <arjunthestar007@gmail.com>
2019-03-09 05:06:31 -0800

    method added

--- app/src/main/java/com/example/arjun/pocasynchronousandroid/MainActivity.java
+++ app/src/main/java/com/example/arjun/pocasynchronousandroid/MainActivity.java
@@ -1,17 +1,41 @@
 package com.example.arjun.pocasynchronousandroid;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class MainActivity extends AppCompatActivity {
     ImageView imageView;
     public static final String url = "https://tineye.com/images/widgets/mona.jpg";
+    Bitmap bitmap;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         imageView=findViewById(R.id.imageView);
+    }
+
+
+    public Bitmap gettheImage() {
+        URL newurl = null;
+        try {
+            newurl = new URL(url);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        try {
+            bitmap = BitmapFactory.decodeStream(newurl.openConnection().getInputStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return bitmap;
     }
 }
