1c9f75d1d25b1799275433cf4832f81b1fc6e121
arjun <arjunthestar007@gmail.com>
2019-03-08 19:11:36 -0800

    imageview added

--- app/src/main/java/com/example/arjun/pocasynchronousandroid/MainActivity.java
+++ app/src/main/java/com/example/arjun/pocasynchronousandroid/MainActivity.java
@@ -2,12 +2,15 @@ package com.example.arjun.pocasynchronousandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageView=findViewById(R.id.imageView);
     }
 }
